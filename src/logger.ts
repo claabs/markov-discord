696a72257e442d715f5802d4faefd6913eeac7c1
Charlie Laabs <charlielaabs@gmail.com>
2022-03-27 19:44:32 -0500

    Refactor logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,21 +1,21 @@
-import pino from 'pino';
 import 'dotenv/config';
+import pino from 'pino';
+import PinoPretty from 'pino-pretty';
 import { config } from './config';
 
-const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      translateTime: `SYS:standard`,
+const logger = pino(
+  {
+    formatters: {
+      level: (label) => {
+        return { level: label };
+      },
     },
+    level: config.logLevel,
+    base: undefined,
   },
-  formatters: {
-    level: (label) => {
-      return { level: label };
-    },
-  },
-  level: config.logLevel,
-  base: undefined,
-});
+  PinoPretty({
+    translateTime: `SYS:standard`,
+  })
+);
 
 export default logger;
